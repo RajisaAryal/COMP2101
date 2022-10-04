7424aaa185a3e1cc174ac285bfc7f0cac5e4deb2
Rajisa248 <you@yourdomain>
2022-10-04 13:06:46 -0400

    I created the second script

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,4 +1,14 @@
+#!/bin/bash
+echo -e "Report for myvm\n==============="
+#Setting the system’s fully-qualified domain name (FQDN)
 echo "FQDN:$(hostname)" 
-echo -e "Host Information:\n $(hostnamectl)"
-echo -e "IP Adresses:\n $(hostname -I && ifconfig ens33 |  awk '/inet6/{print $2}')"
-echo -e "Root Filesystem Status:\n $(df /dev/sda3)"
+
+#Setting  the  operating system name and version,
+echo -e "Operating system name and Version: $(lsb_release -d | awk '{print $2,$3'})"
+
+#Setting any IP addresses the machine has; 
+echo -e "IP Adresses: $(hostname -I)"
+
+#Setting the amount of space available in only the root filesystem
+echo -e "Root Filesystem free space : $(df -h /dev/sda3 | awk '{print $4'}| tail -1)"
+echo -e "================"
