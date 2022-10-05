7c26a116cca80c836eebbb748f5e811f11b4e803
Rajisa248 <you@yourdomain>
2022-10-05 10:48:25 -0400

    This is my second script

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo -e "Report for myvm\n==============="
 #Setting the system’s fully-qualified domain name (FQDN)
-echo "FQDN:$(hostname)" 
+echo "FQDN:$(hostname)"
 
 #Setting  the  operating system name and version,
 echo -e "Operating system name and Version: $(lsb_release -d | awk '{print $2,$3'})"
 
-#Setting any IP addresses the machine has; 
+#Setting any IP addresses the machine has;
 echo -e "IP Adresses: $(hostname -I)"
 
 #Setting the amount of space available in only the root filesystem
